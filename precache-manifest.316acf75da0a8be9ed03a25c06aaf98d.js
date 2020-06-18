675693ab1d3da18dfbf2b6ae96ea716a14334903
Alex Aragon <ismalex@gmail.com>
2020-06-19 00:09:33 +0100

    Updates

---
+++ precache-manifest.316acf75da0a8be9ed03a25c06aaf98d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3dc681a74f93a36aa67efd0458105d",
+    "url": "/COVID--Worldwide/index.html"
+  },
+  {
+    "revision": "d0e4fcfd4f977848ce83",
+    "url": "/COVID--Worldwide/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "253c50f0cf83f167c725",
+    "url": "/COVID--Worldwide/static/js/2.47da7df1.chunk.js"
+  },
+  {
+    "revision": "069e703a7ca014ab76714f20e2a39562",
+    "url": "/COVID--Worldwide/static/js/2.47da7df1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0e4fcfd4f977848ce83",
+    "url": "/COVID--Worldwide/static/js/main.d92c4090.chunk.js"
+  },
+  {
+    "revision": "9852006610a7b7ecb28e",
+    "url": "/COVID--Worldwide/static/js/runtime-main.7580b69f.js"
+  }
+]);
